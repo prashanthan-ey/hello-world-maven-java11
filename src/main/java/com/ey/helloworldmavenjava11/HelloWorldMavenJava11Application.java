8742ed648631a7b6be8e27f7587614cff8000331
Prashanthan <Prashanthan.Subramaniyam@gds.ey.com>
2023-01-25 01:07:29 +0530

    updated war

--- src/main/java/com/ey/helloworldmavenjava11/HelloWorldMavenJava11Application.java
+++ src/main/java/com/ey/helloworldmavenjava11/HelloWorldMavenJava11Application.java
@@ -2,9 +2,10 @@ package com.ey.helloworldmavenjava11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldMavenJava11Application {
+public class HelloWorldMavenJava11Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldMavenJava11Application.class, args);
